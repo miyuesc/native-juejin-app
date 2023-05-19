515c6385a580ea7b35ac14572eed64d0f75a5e52
MiyueSC <913784771@qq.com>
2023-05-19 16:31:42 +0800

    feat: 小册

--- pages/books/books.js
+++ pages/books/books.js
@@ -1,18 +1,62 @@
 // pages/books/books.js
+import {
+  getBooksList
+} from "../../requests/books.js"
+
+const buyTypeMap = {
+  1: '已购',
+  2: '免费',
+  4: '借阅'
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    booksList: []
+  },
 
+  // methods
+  async getBooks() {
+    try {
+      const { data } = await getBooksList()
+      console.log(data)
+      this.setData({ booksList: data.map(this.getBookInfo) })
+    } catch (e) {
+      this.setData({ booksList: [] })
+    }
+  },
+
+  getBookInfo(item) {
+    const borrowingExpireTime = item.reading_progress.valid_end_time;
+    return {
+      bookId: item.base_info.booklet_id,
+      bookImage: item.base_info.cover_img,
+      bookName: item.base_info.title,
+      bookSummary: item.base_info.summary,
+      bookAuthor: item.user_info.user_name,
+      bookAuthorIcon: item.user_info.avatar_large,
+      bookAuthorCompany: item.user_info.company,
+      bookAuthorJobTitle: item.user_info.job_title,
+
+      isBuy: item.is_buy,
+      isNew: item.is_new,
+
+      statusLabel: buyTypeMap[item.reading_progress.buy_type],
+      totalSectionNum: 20,
+      currentSectionId: item.reading_progress.last_section_id,
+      readingProgress: item.reading_progress.reading_progress,
+      borrowingExpireTime: borrowingExpireTime === 0 ? '' : new Date(borrowingExpireTime * 1000).toLocaleDateString()
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.getBooks()
   },
 
   /**
@@ -47,7 +91,8 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    this.setData({ booksList: [] });
+    this.getBooks().then(wx.stopPullDownRefresh)
   },
 
   /**
---
+++ requests/books.js
@@ -0,0 +1,13 @@
+import { postData } from "../utils/request.js"
+
+export function getBooksList() {
+	return postData('/booklet_api/v1/booklet/bookletshelflist', {}, {aid: 2608, uuid: '7215848128545064463', splider: 0})
+}
+
+export function getBookTOC(booklet_id) {
+	return postData('/booklet_api/v1/booklet/get', {booklet_id})
+}
+
+export function getBookSection(section_id) {
+	return postData('/booklet_api/v1/booklet/bookletshelflist', {section_id})
+}
